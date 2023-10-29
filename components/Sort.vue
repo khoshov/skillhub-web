b9d0430b1498ac1afbcb4d7def59939337d42ef4
stanislavkhoshov <khoshov@gmail.com>
2023-10-29 22:22:52 +0400

    init vuetify

--- components/Sort.vue
+++ components/Sort.vue
@@ -1,36 +1,25 @@
 <template>
-  <UDropdown :items="items" :popper="{ placement: 'bottom-start' }">
-    Сначала
-    <UButton class="ui-btn" color="white" trailing-icon="i-heroicons-chevron-down-20-solid"/>
-  </UDropdown>
+  <v-select
+    @update:menu="doSomething"
+    v-model=value
+    :items="items"
+    variant="outlined"
+  ></v-select>
 </template>
 
 <script setup>
-import {useCourseStore} from "../stores/course";
+// import {useCourseStore} from "../stores/course";
+// const courseStore = useCourseStore();
+// courseStore.setOrder('popular')
+// courseStore.getCourses()
 
-const courseStore = useCourseStore();
-const items = [
-  [{
-    label: 'Популярные',
-    click: () => {
-      courseStore.setOrder('popular')
-      courseStore.getCourses()
-    }
-  }, {
-    label: 'Бесплатные',
-  }, {
-    label: 'Дешевые',
-  }, {
-    label: 'Дорогие',
-  }, {
-    label: 'Короткие',
-  }, {
-    label: 'Долгие',
-  },
-    {
-      label: 'Высокий рейтинг',
-    }]
-]
+const items = ['Популярные', 'Бесплатные', 'Дешевые', 'Дорогие', 'Короткие', 'Долгие', 'Высокий рейтинг']
+
+let value = items[0]
+
+const doSomething = (e) => {
+  console.log(e)
+}
 </script>
 
 <style lang="scss" scoped>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,21 +1,39 @@
+import vuetify, {transformAssetUrls} from 'vite-plugin-vuetify'
+
 export default defineNuxtConfig({
-  devtools: { enabled: true },
-  modules: [
-      '@pinia/nuxt',
-  ],
-  css: ['~/assets/scss/style.scss'],
-  script: [
-    {
-      src: 'bootstrap/dist/js/bootstrap.bundle.min.js'
-    }
-  ],
-  meta: {
-    title: "Skillhub",
-  },
-  runtimeConfig: {
-    public: {
-      apiBase: process.env.NUXT_PUBLIC_API_BASE || '/api'
-    }
-  },
-  buildModules: ['@nuxtjs/date-fns']
+    build: {
+        transpile: ['vuetify'],
+    },
+    devtools: {enabled: true},
+    modules: [
+        '@pinia/nuxt',
+        (_options, nuxt) => {
+            nuxt.hooks.hook('vite:extendConfig', (config) => {
+                // @ts-expect-error
+                config.plugins.push(vuetify({autoImport: true}))
+            })
+        },
+    ],
+    css: ['~/assets/scss/style.scss'],
+    script: [
+        {
+            src: 'bootstrap/dist/js/bootstrap.bundle.min.js'
+        }
+    ],
+    meta: {
+        title: "Skillhub",
+    },
+    runtimeConfig: {
+        public: {
+            apiBase: process.env.NUXT_PUBLIC_API_BASE || '/api'
+        }
+    },
+    buildModules: ['@nuxtjs/date-fns'],
+    vite: {
+        vue: {
+            template: {
+                transformAssetUrls,
+            },
+        },
+    },
 })
